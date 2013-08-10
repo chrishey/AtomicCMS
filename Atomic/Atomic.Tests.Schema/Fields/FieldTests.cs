a87704d0cc7d542326d041dc8726a953e17eaeeb
Richard Read <rickliveshere@yahoo.com>
2013-08-10 20:12:48 +0100

    Defining schemas, fields and field types
    Initial interface implementation of:
    - IObject - everything in Atomic is inheritable from IObject
    - ISchema - defines a schema, structured by it's collection of fields
    - IField - defines a field that can be as part of a schema
    - IFieldType - defines a type of field

---
+++ Atomic/Atomic.Tests.Schema/Fields/FieldTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Atomic.Tests.Model.Fields
+{
+    /// <summary>
+    /// Tests (acting as requirements) for determining how a developer is able to define their own
+    /// fields that can be used to structure schemas.
+    /// </summary>
+    public class FieldTests
+    {
+        public FieldTests()
+        {
+        }
+    }
+}
